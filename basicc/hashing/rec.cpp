598eba639ebf19671171b0b66b64e1b8e73ec524
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-08-09 01:09:24 +0530

    pattern

---
+++ basicc/hashing/rec.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char name = 'a';
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=5;j++)
+        {
+           cout<< char name +1<< <<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
