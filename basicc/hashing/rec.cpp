1741226b43947ac64db4043a510fd536516b2407
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-08-09 15:01:18 +0530

    getl

--- basicc/hashing/rec.cpp
+++ basicc/hashing/rec.cpp
@@ -2,14 +2,8 @@
 using namespace std;
 int main()
 {
-    char name = 'a';
-    for(int i=1;i<=5;i++)
+    for(int i=1;i<5;i++)
     {
-        for(int j=1;j<=5;j++)
-        {
-           cout<< char name +1<< <<" ";
-        }
-        cout<<endl;
+        for(int j =)
     }
-    return 0;
 }
