85dd91c5d46b6c1cfe9084ff9f1279d847740fbf
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-08-14 21:45:16 +0530

    nearest greatest element

---
+++ ngtleft.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>arr = {1,2,3,4,5};
+    int n = arr.size();
+    vector<int>ans(n,-1);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j] > arr[i])
+            {
+                ans[i] = arr[j];
+                break;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<ans[i]<<"  ";
+    }
+    return 0;
+}
